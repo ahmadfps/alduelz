56187f838ed36fb53f027559e5bdd3b069c06fbb
ahmadfps <zomic46@gmail.com>
2026-06-20 22:28:59 +0500

    alduelz

---
+++ commands/💪 Setup/setup-commands.js
@@ -0,0 +1,189 @@
+var { MessageEmbed } = require(`discord.js`);
+var Discord = require(`discord.js`);
+var config = require(`${process.cwd()}/botconfig/config.json`);
+var ee = require(`${process.cwd()}/botconfig/embed.json`);
+var emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+var { databasing } = require(`${process.cwd()}/handlers/functions`);
+const { MessageButton, MessageActionRow, MessageSelectMenu } = require("discord.js");
+const { allEmojis } = require("../../botconfig/emojiFunctions");
+module.exports = {
+    name: "setup-commands",
+    category: "💪 Setup",
+    aliases: ["setupcommands", "setup-command", "setupcommand"],
+    cooldown: 5,
+    usage: "setup-commands  -->  Follow the Steps",
+    description: "Enable/Disable specific Commands",
+    memberpermissions: ["ADMINISTRATOR"],
+    type: "info",
+    run: async (client, message, args, cmduser, text, prefix) => {
+        let es = client.settings.get(message.guild.id, "embed");
+        let ls = client.settings.get(message.guild.id, "language");
+        try {
+            function getMenuOptions() {
+                return [
+                    {
+                        label: "ECONOMY",
+                        value: "ECONOMY",
+                        emoji: "💸",
+                        description: `${client.settings.get(message.guild.id, "ECONOMY") ? "❌ Disable ECONOMY Commands" : "✅ Enable ECONOMY Commands"}`,
+                    },
+                    {
+                        label: "SCHOOL",
+                        value: "SCHOOL",
+                        emoji: "🏫",
+                        description: `${client.settings.get(message.guild.id, "SCHOOL") ? "❌ Disable SCHOOL Commands" : "✅ Enable SCHOOL Commands"}`,
+                    },
+                    {
+                        label: "MUSIC",
+                        value: "MUSIC",
+                        emoji: "🎶",
+                        description: `${client.settings.get(message.guild.id, "MUSIC") ? "❌ Disable Music Commands" : "✅ Enable Music Commands"}`,
+                    },
+                    {
+                        label: "FILTER",
+                        value: "FILTER",
+                        emoji: "👀",
+                        description: `${client.settings.get(message.guild.id, "FILTER") ? "❌ Disable FILTER Commands" : "✅ Enable FILTER Commands"}`,
+                    },
+                    {
+                        label: "CUSTOMQUEUE",
+                        value: "CUSTOMQUEUE",
+                        emoji: "⚜️",
+                        description: `${client.settings.get(message.guild.id, "CUSTOMQUEUE") ? "❌ Disable CUSTOM-QUEUE Commands" : "✅ Enable CUSTOM-QUEUE Commands"}`,
+                    },
+                    {
+                        label: "PROGRAMMING",
+                        value: "PROGRAMMING",
+                        emoji: "⌨️",
+                        description: `${client.settings.get(message.guild.id, "PROGRAMMING") ? "❌ Disable PROGRAMMING Commands" : "✅ Enable PROGRAMMING Commands"}`,
+                    },
+                    {
+                        label: "RANKING",
+                        value: "RANKING",
+                        emoji: "📈",
+                        description: `${client.settings.get(message.guild.id, "RANKING") ? "❌ Disable RANKING Commands" : "✅ Enable RANKING Commands"}`,
+                    },
+                    {
+                        label: "SOUNDBOARD",
+                        value: "SOUNDBOARD",
+                        emoji: "🔊",
+                        description: `${client.settings.get(message.guild.id, "SOUNDBOARD") ? "❌ Disable SOUNDBOARD Commands" : "✅ Enable SOUNDBOARD Commands"}`,
+                    },
+                    {
+                        label: "VOICE",
+                        value: "VOICE",
+                        emoji: "🎤",
+                        description: `${client.settings.get(message.guild.id, "VOICE") ? "❌ Disable VOICE Commands" : "✅ Enable VOICE Commands"}`,
+                    },
+                    {
+                        label: "FUN",
+                        value: "FUN",
+                        emoji: "🕹️",
+                        description: `${client.settings.get(message.guild.id, "FUN") ? "❌ Disable FUN Commands" : "✅ Enable FUN Commands"}`,
+                    },
+                    {
+                        label: "MINIGAMES",
+                        value: "MINIGAMES",
+                        emoji: "🎮",
+                        description: `${client.settings.get(message.guild.id, "MINIGAMES") ? "❌ Disable MINIGAMES Commands" : "✅ Enable MINIGAMES Commands"}`,
+                    },
+                    {
+                        label: "ANIME",
+                        value: "ANIME",
+                        emoji: "😳",
+                        description: `${client.settings.get(message.guild.id, "ANIME") ? "❌ Disable ANIME Commands" : "✅ Enable ANIME Commands"}`,
+                    },
+                    {
+                        label: "NSFW",
+                        value: "NSFW",
+                        emoji: "🔞",
+                        description: `${client.settings.get(message.guild.id, "NSFW") ? "❌ Disable NSFW Commands" : "✅ Enable NSFW Commands"}`,
+                    },
+                ];
+            }
+            function getMenuRowComponent() {
+                let menuOptions = getMenuOptions();
+                let menuSelection = new MessageSelectMenu()
+                    .setCustomId("MenuSelection")
+                    .setPlaceholder("Click: enable/disable Command-Categories")
+                    .setMinValues(1)
+                    .setMaxValues(menuOptions.length)
+                    .addOptions(menuOptions.filter(Boolean));
+                return [new MessageActionRow().addComponents(menuSelection)];
+            }
+
+            let embed = new Discord.MessageEmbed()
+                .setTitle(`Setup the allowed/not-allowed Command-Categories of this Server`)
+                .setColor(es.color)
+                .setDescription(
+                    `**In the selection down below all Categories are listed**\n\n**Select it to either disable/enable it!**\n\n**You can select all (*at least 1*) Command-Categories if you want to disable/enable all of them at once!**`
+                );
+
+            //Send message with buttons
+            let msg = await message.reply({
+                embeds: [embed],
+                components: getMenuRowComponent(),
+            });
+            const collector = msg.createMessageComponentCollector({
+                filter: i => i?.isSelectMenu() && i?.user && i?.message.author.id == client.user.id,
+                time: 180e3,
+                max: 1,
+            });
+            collector.on("collect", async b => {
+                if (b?.user.id !== message.author.id)
+                    return b?.reply({
+                        content: `${allEmojis.msg.ERROR} Only the one who typed the Command is allowed to select Things!`,
+                        ephemeral: true,
+                    });
+
+                let enabled = 0,
+                    disabled = 0;
+                for (const value of b?.values) {
+                    let oldstate = client.settings.get(message.guild.id, `${value.toUpperCase()}`);
+                    if (!oldstate) enabled++;
+                    else disabled++;
+                    client.settings.set(message.guild.id, !oldstate, `${value.toUpperCase()}`);
+                }
+                b?.reply(
+                    `${allEmojis.msg.SUCCESS} **\`Enabled ${enabled} Command-Categories\` and \`Disabled ${disabled} Command-Categories\` out of \`${b?.values.length} selected Command-Categories\`**`
+                );
+            });
+            collector.on("end", () => {
+                msg.edit({
+                    content: `${allEmojis.msg.ERROR} Time ran out/Input finished! Cancelled`,
+                    embeds: [
+                        msg.embeds[0].setDescription(
+                            `${getMenuOptions()
+                                .map(
+                                    option =>
+                                        `> ${option.emoji} **${option.value}-Commands**: ${option.description.split(" ")[0] != "❌" ? `\`Are now disabled [❌]\`` : `\`Are now enabled [✅]\``}`
+                                )
+                                .join("\n\n")}`
+                        ),
+                    ],
+                    components: [],
+                }).catch(e => {});
+            });
+        } catch (e) {
+            console.log(String(e.stack).grey.bgRed);
+            return message.reply({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor(es.wrongcolor)
+                        .setFooter(client.getFooter(es))
+                        .setTitle(client.la[ls].common.erroroccur)
+                        .setDescription(eval(client.la[ls]["cmds"]["setup"]["setup-commands"]["variable5"])),
+                ],
+            });
+        }
+    },
+};
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
---
+++ commands/💸 Economy/storeinfo.js
@@ -0,0 +1,186 @@
+const { MessageEmbed } = require("discord.js");
+const { allEmojis } = require("../../botconfig/emojiFunctions");
+const config = require(`${process.cwd()}/botconfig/config.json`);
+var ee = require(`${process.cwd()}/botconfig/embed.json`);
+const emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+const { parseMilliseconds, duration, GetUser, nFormatter, ensure_economy_user } = require(
+    `${process.cwd()}/handlers/functions`
+);
+module.exports = {
+    name: "storeinfo",
+    category: "💸 Economy",
+    aliases: ["store", "shop"],
+    description: "Shows the Store",
+    usage: "storeinfo",
+    type: "info",
+    run: async (client, message, args, cmduser, text, prefix) => {
+        let es = client.settings.get(message.guild.id, "embed");
+        let ls = client.settings.get(message.guild.id, "language");
+        if (!client.settings.get(message.guild.id, "ECONOMY")) {
+            return message.reply({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor(es.wrongcolor)
+                        .setFooter(client.getFooter(es))
+                        .setTitle(client.la[ls].common.disabled.title)
+                        .setDescription(eval(client.la[ls]["cmds"]["economy"]["storeinfo"]["variable1"])),
+                ],
+            });
+        }
+        try {
+            //command
+            var user = message.author;
+            if (user.bot) return message.reply(eval(client.la[ls]["cmds"]["economy"]["storeinfo"]["variable2"]));
+
+            //ensure the economy data
+            ensure_economy_user(client, message.guild.id, user.id);
+            const data = client.economy.get(`${message.guild.id}-${user.id}`);
+            var items = 0;
+            var itemsvalue = 0;
+            for (const itemarray in data.items) {
+                items += data.items[`${itemarray}`];
+                var prize = 0;
+                switch (itemarray.toLowerCase()) {
+                    case "yacht":
+                        prize = 75000;
+                        break;
+                    case "lamborghini":
+                        prize = 50000;
+                        break;
+                    case "car":
+                        prize = 6400;
+                        break;
+                    case "motorbike":
+                        prize = 1500;
+                        break;
+                    case "bicycle":
+                        prize = 500;
+                        break;
+
+                    case "nike":
+                        prize = 300;
+                        break;
+                    case "tshirt":
+                        prize = 60;
+                        break;
+
+                    case "mansion":
+                        prize = 45000;
+                        break;
+                    case "house":
+                        prize = 8000;
+                        break;
+                    case "dirthut":
+                        prize = 150;
+                        break;
+
+                    case "pensil":
+                        prize = 20;
+                        break;
+                    case "pen":
+                        prize = 10;
+                        break;
+                    case "condom":
+                        prize = 30;
+                        break;
+                    case "bottle":
+                        prize = 50;
+                        break;
+
+                    case "fish":
+                        prize = 1000;
+                        break;
+                    case "hamster":
+                        prize = 1500;
+                        break;
+                    case "dog":
+                        prize = 2000;
+                        break;
+                    case "cat":
+                        prize = 2000;
+                        break;
+                }
+                itemsvalue += prize * data.items[`${itemarray}`];
+            }
+            const p2b = costs => (Number(costs) > Number(data.balance) ? allEmojis.msg.ERROR : allEmojis.msg.SUCCESS);
+            //return some message!
+            return message.reply({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor(es.color)
+                        .setThumbnail(
+                            es.thumb
+                                ? es.footericon && (es.footericon.includes("http://") || es.footericon.includes("https://"))
+                                    ? es.footericon
+                                    : client.user.displayAvatarURL()
+                                : null
+                        )
+                        .setFooter(
+                            user.tag + " | ❌ .. Unable to buy | ✅ ... Possible to buy",
+                            user.displayAvatarURL({ dynamic: true })
+                        )
+                        .setTitle(eval(client.la[ls]["cmds"]["economy"]["storeinfo"]["variable3"]))
+                        .setDescription(eval(client.la[ls]["cmds"]["economy"]["storeinfo"]["variable4"]))
+                        .addField(
+                            "✏️ Useables",
+                            ">>> " +
+                                `✏️ **\`Pensil [10 💸]\`** | ${p2b(10)}
+🖊️ **\`Pen [20 💸]\`** | ${p2b(20)}
+🟪 **\`Condom [30 💸]\`** | ${p2b(30)}
+🍼 **\`Bottle [50 💸]\`** | ${p2b(50)}`
+                        )
+                        .addField(
+                            "👕 Clothes",
+                            ">>> " +
+                                `👟 **\`Nike Shoe [300 💸]\`** | ${p2b(300)}
+👕 **\`T-Shirt [60 💸]\`** | ${p2b(60)}`
+                        )
+                        .addField(
+                            "🐕 Animals",
+                            ">>> " +
+                                `🐟\`Fish [1000 💸]\`** | ${p2b(1000)}
+🐹 **\`Hamster [1500 💸]\`** | ${p2b(1500)}
+🐕 **\`Dog [2000 💸]\`** | ${p2b(2000)}
+😺 **\`Cat [2000 💸]\`** | ${p2b(2000)}`
+                        )
+                        .addField(
+                            "🚗 Means of Transport",
+                            ">>> " +
+                                `🛥️\`Yacht [75000 💸]\`** | ${p2b(75000)}
+🏎️ **\`Lamborghini [50000 💸]\`** | ${p2b(50000)}
+🚗 **\`Car [6400 💸]\`** | ${p2b(6400)}
+🏍️ **\`Motorbike [1500 💸]\`** | ${p2b(1500)}
+🚲 **\`Bicycle [500 💸]\`** | ${p2b(500)}`
+                        )
+                        .addField(
+                            "🏠 Livingarea",
+                            ">>> " +
+                                `🏘️ **\`Mansion [45000 💸]\`** | ${p2b(45000)}
+🏠 **\`House [8000 💸]\`** | ${p2b(8000)}
+🟫 **\`Dirthut [150 💸]\`** | ${p2b(150)}`
+                        ),
+                ],
+            });
+        } catch (e) {
+            console.log(String(e.stack).grey.bgRed);
+            return message.reply({
+                embeds: [
+                    new MessageEmbed()
+                        .setColor(es.wrongcolor)
+                        .setFooter(client.getFooter(es))
+                        .setTitle(client.la[ls].common.erroroccur)
+                        .setDescription(eval(client.la[ls]["cmds"]["economy"]["storeinfo"]["variable5"])),
+                ],
+            });
+        }
+    },
+};
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
